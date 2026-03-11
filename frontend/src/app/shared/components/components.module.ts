1b638aa4c01fd99ca342c597372b588cffddd5b2
zoubidabenlem <zoubidabenlem@gmail.com>
2026-03-11 23:04:36 +0000

    FEAT- FRONTEND : created landing page and global components

--- frontend/src/app/shared/components/components.module.ts
+++ frontend/src/app/shared/components/components.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartComponent } from './chart/chart.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    ChartComponent
+    ChartComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
